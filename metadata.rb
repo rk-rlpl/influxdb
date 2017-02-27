eac314e71c82208632f29d37686f2db0759b5b64
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-02-27 16:48:49 +0530

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'influxdb'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures influxdb'
+long_description 'Installs/Configures influxdb'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/influxdb/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/influxdb' if respond_to?(:source_url)
