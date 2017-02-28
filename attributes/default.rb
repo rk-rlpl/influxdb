fd7e0788cae3c536eab82681dd6f5a49e404f0aa
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-02-28 14:13:51 +0530

    Modify Naming

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['OS']['DISTRIB_ID'] = 'ubuntu'
-default['OS']['DISTRIB_RELEASE'] = '16.04'
-default['OS']['DISTRIB_CODENAME'] = 'xenial'
-default['OS']['DISTRIB_DESCRIPTION'] = "Ubuntu 16.04.2 LTS"
+default['Ubuntu']['DISTRIB_ID'] = 'ubuntu'
+default['Ubuntu']['DISTRIB_RELEASE'] = '16.04'
+default['Ubuntu']['DISTRIB_CODENAME'] = 'xenial'
+default['Ubuntu']['DISTRIB_DESCRIPTION'] = "Ubuntu 16.04.2 LTS"
