18b6d09bfab6118b5e5caf0e3508a7793cd2ff06
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-02-28 12:29:04 +0530

    install influxdb for centos

--- recipes/install.rb
+++ recipes/install.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 if node['platform'] == 'ubuntu'
-  
+
   package 'apt-transport-https'
 
   apt_repository 'influxdb' do
@@ -16,6 +16,23 @@ if node['platform'] == 'ubuntu'
   apt_update 'all platforms' do
     frequency 86400
     action :periodic
+  end
+
+  package 'influxdb'
+
+  service 'influxdb' do
+    action [:enable, :start]
+  end
+
+end
+
+if node['platform'] == 'centos'
+
+  yum_repository 'influxdb' do
+    description 'Influxdb Repo'
+    baseurl 'https://repos.influxdata.com/rhel/$releasever/$basearch/stable'
+    gpgkey 'https://repos.influxdata.com/influxdb.key'
+    action :create
   end
 
   package 'influxdb'
