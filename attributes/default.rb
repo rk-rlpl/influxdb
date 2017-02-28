8efe2f4a0964192b5a964fedb96cd8ab75e9d629
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-02-28 12:06:36 +0530

    added attributes for influxdb

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['OS']['DISTRIB_ID'] = 'ubuntu'
+default['OS']['DISTRIB_RELEASE'] = '16.04'
+default['OS']['DISTRIB_CODENAME'] = 'xenial'
+default['OS']['DISTRIB_DESCRIPTION'] = "Ubuntu 16.04.2 LTS"
